1245806acfdcdb90a5be0f882247fdb91c97aace
RJie <1542235034@qq.com>
2020-04-14 14:20:11 +0800

    网页授权

--- src/WechatServer.php
+++ src/WechatServer.php
@@ -24,6 +24,11 @@ class WechatServer
         $this->client = new Client();
     }
 
+    /**
+     * 获取微信公众号access_token
+     *
+     * @return mixed
+     */
     public function getAccessToken()
     {
         $config = $this->config;
@@ -37,5 +42,59 @@ class WechatServer
 
             return $result['access_token'];
         });
+    }
+
+    /**
+     * 网页授权，用户同意获取code的路径
+     *
+     * @param $redirect_uri
+     * @param string $scope
+     * @param string $state
+     * @return string
+     */
+    public function getCodeUri($redirect_uri, $scope = 'snsapi_base', $state = 'STATE')
+    {
+        $api_url = 'https://open.weixin.qq.com/connect/oauth2/authorize?appid=' . $this->config['appID'] . '&redirect_uri=' . urlencode($redirect_uri) . '&response_type=code&scope=' . $scope . '&state=' . $state . '&connect_redirect=1#wechat_redirect';
+
+        return $api_url;
+    }
+
+    /**
+     * 网页授权，用户同意获取code
+     *
+     * @param $redirect_uri
+     * @param string $scope
+     * @param string $state
+     */
+    public function getCode($redirect_uri, $scope = 'snsapi_base', $state = 'STATE')
+    {
+        $api_url = $this->getCodeUri($redirect_uri, $scope = 'snsapi_base', $state = 'STATE');
+        header('Location:' . $api_url);
+    }
+
+    /**
+     * 网页授权，通过code换取网页授权access_token
+     *
+     * @param $code
+     * @return mixed
+     */
+    public function getCodeToken($code)
+    {
+        if ($code) {
+            $api_url = 'https://api.weixin.qq.com/sns/oauth2/access_token?';
+            $api_url = $api_url . 'appid=' . $this->appid;
+            $api_url = $api_url . '&secret=' . $this->secret;
+            $api_url = $api_url . '&code=' . $code;
+            $api_url = $api_url . '&grant_type=authorization_code';
+
+            $respond = $this->client->request('get',$api_url);
+            if ($respond->getStatusCode() === 200) {
+                return json_decode($respond->getBody()->getContents(), true);
+            }else{
+                throw new \LogicException('请求失败');
+            }
+        } else {
+            throw new \LogicException('code为空');
+        }
     }
 }
